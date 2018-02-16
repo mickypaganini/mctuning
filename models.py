48530704f2c5e97e18447406e1ab190b61dfb927
Michela Paganini <micky.91@hotmail.com>
2018-02-16 13:57:42 -0800

    Add custom LSTM init, remove old rnn implementation

--- models.py
+++ models.py
@@ -87,6 +87,8 @@ class DoubleLSTM(nn.Module):
             bidirectional=bidirectional
         )
 
+        self.init_lstm()
+
         # output dense layer
         self.dense = nn.Linear(
             # (2 *) because of torch.cat; (* 2) because of 2 streams
@@ -95,6 +97,20 @@ class DoubleLSTM(nn.Module):
         )
 
         self.dense2 = nn.Linear(128, tagger_output_size)
+
+    def init_lstm(self):
+        for l in [self.lstm_lead, self.lstm_sublead]:
+            # recurrent kernel init
+            nn.init.orthogonal(l.weight_hh_l0)
+
+            # in -> h kernel init
+            nn.init.xavier_uniform(l.weight_ih_l0)
+
+
+            # Initialize the forget gate to one
+            for bias in [l.bias_hh_l0, l.bias_ih_l0]:
+                bias.data.zero_()
+                bias.data[l.hidden_size:2 * l.hidden_size] = 1.0
 
     def forward(self, leading_jets, subleading_jets, lengths,
                 batch_weights, batch_size):
@@ -164,96 +180,5 @@ class DoubleLSTM(nn.Module):
         batch_std = batch_second_moment - torch.pow(batch_mean, 2)
 
         batch_features = torch.cat([batch_mean, batch_std], 1)
-        outputs = self.dense2(F.relu(self.dense(batch_features)))
-        return outputs
-
-
-class OldDoubleLSTM(nn.Module):
-
-    def __init__(self, input_size, output_size, num_layers, dropout, bidirectional,
-                 batch_size, tagger_output_size):
-        super(DoubleLSTM, self).__init__()
-        
-        # members
-        self.input_size = input_size
-        self.output_size = output_size
-        self.num_layers = num_layers
-        self.num_directions = 2 if bidirectional else 1
-        self.batch_size = batch_size
-
-        # LSTM layers
-        self.lstm_lead = nn.LSTM(input_size=input_size, hidden_size=output_size,
-                                 num_layers=num_layers, batch_first=True,
-                                 dropout=dropout, bidirectional=bidirectional)
-        
-        self.lstm_sublead = nn.LSTM(input_size=input_size, hidden_size=output_size,
-                                    num_layers=num_layers, batch_first=True,
-                                    dropout=dropout, bidirectional=bidirectional)
-        
-        # output dense layer
-        self.dense = nn.Linear(
-            # (2 *) because of torch.cat; (* 2) because of 2 streams
-            2 * (output_size * self.num_directions * 2), 
-            128)
-        self.dense2 = nn.Linear(128, tagger_output_size)
-
-    def forward(self, leading_jets, subleading_jets, lengths, batch_weights, batch_size):
-        
-        # sort the data by length of sequence in decreasing order (because PyTorch)
-        leading_jets = leading_jets[np.argsort(lengths[:, 0])[::-1]]
-        subleading_jets = subleading_jets[np.argsort(lengths[:, 1])[::-1]]
-        sorted_lengths = np.sort(lengths, axis=0)[::-1]
-        sortback_leading = np.argsort([np.argsort(lengths[:, 0])[::-1]])[0]
-        sortback_subleading = np.argsort([np.argsort(lengths[:, 1])[::-1]])[0]
-
-        if torch.cuda.is_available():
-            leading_jets.cuda()
-            subleading_jets.cuda()
-
-        # pack sequences
-        packed_leading_jets = nn.utils.rnn.pack_padded_sequence(
-            leading_jets, batch_first=True, lengths=sorted_lengths[:, 0]
-        )
-        packed_subleading_jets = nn.utils.rnn.pack_padded_sequence(
-            subleading_jets, batch_first=True, lengths=sorted_lengths[:, 1]
-        )
-
-        # LSTMs
-        lstm_lead_out, _ = self.lstm_lead(
-            packed_leading_jets)
-        lstm_sublead_out, _= self.lstm_sublead(
-            packed_subleading_jets)
-               
-        # unpack sequences
-        lstm_lead_out, _ = pad_packed_sequence(lstm_lead_out, batch_first=True)
-        lstm_sublead_out, _ = pad_packed_sequence(lstm_sublead_out, batch_first=True)
-
-        # sort back
-        lstm_lead_out = lstm_lead_out[sortback_leading]
-        lstm_sublead_out = lstm_sublead_out[sortback_subleading]
-        
-        # return output of last timestep (not full sequence)
-        if torch.cuda.is_available():
-            lead_len = torch.from_numpy(lengths[:, 0] - 1).type(torch.cuda.LongTensor)
-            sublead_len = torch.from_numpy(lengths[:, 1] - 1).type(torch.cuda.LongTensor)
-        else:
-            lead_len = torch.from_numpy(lengths[:, 0] - 1).type(torch.LongTensor)
-            sublead_len = torch.from_numpy(lengths[:, 1] - 1).type(torch.LongTensor)
-        idx_lead = Variable(
-            lead_len, requires_grad=False).view(-1, 1).expand(lstm_lead_out.size(0), lstm_lead_out.size(2)).unsqueeze(1)
-        lstm_lead_out = lstm_lead_out.gather(1, idx_lead).squeeze()
-        idx_sublead = Variable(
-            sublead_len, requires_grad=False).view(-1, 1).expand(lstm_sublead_out.size(0), lstm_sublead_out.size(2)).unsqueeze(1)
-        lstm_sublead_out = lstm_sublead_out.gather(1, idx_sublead).squeeze()
-        
-        # concatenate outputs of the 2 LSTMs
-        hidden = torch.cat([lstm_lead_out, lstm_sublead_out], 1)
-        hidden = F.relu(hidden)
-
-        batch_mean = batch_weights.mm(hidden).expand(batch_size, hidden.shape[-1])
-        batch_second_moment = batch_weights.mm(torch.pow(hidden, 2)).expand(batch_size, hidden.shape[-1])
-        batch_std = batch_second_moment - torch.pow(batch_mean, 2)
-
-        batch_features = torch.cat([batch_mean, batch_std], 1)    
         outputs = self.dense2(F.relu(self.dense(batch_features)))
         return outputs
