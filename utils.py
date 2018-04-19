230dacd63eb96d982a4f80ed76265c685531c0cd
Michela Paganini <micky.91@hotmail.com>
2018-04-19 06:13:12 -0700

    Set logging to info

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ from errno import EEXIST
 
 def configure_logging():
     logger = logging.getLogger()
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(logging.INFO)
     logging.basicConfig(format="%(levelname)-8s\033[1m%(name)-21s%(process)d\033[0m: %(message)s") 
     logging.addLevelName(logging.WARNING, "\033[1;31m{:8}\033[1;0m".format(logging.getLevelName(logging.WARNING)))
     logging.addLevelName(logging.ERROR, "\033[1;35m{:8}\033[1;0m".format(logging.getLevelName(logging.ERROR)))
